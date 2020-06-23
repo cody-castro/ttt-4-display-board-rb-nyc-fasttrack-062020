41976671e331b80f24232d028b2e780bc59fd35f
Cody C <codyicastro@gmail.com>
2020-06-23 04:02:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = ["   ", "    ", "   ", "   ", "   ", "   ", "   ", "   ", "    "]
+rows = ["  |  |  ", "-----------", "  |  |  ", "-----------", "   |   |   "]
+
+
+
+def display_board(board = "  |  |  ", "-----------", "  |  |  ", "-----------", "   |   |   " )
+  puts board
+end
+
+
+puts display_board(board)
+
