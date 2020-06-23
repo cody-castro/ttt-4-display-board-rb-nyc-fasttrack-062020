f73fc33b144ae27d1a7f4a5e2f2131807199e22d
Cody C <codyicastro@gmail.com>
2020-06-23 04:56:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,9 +9,9 @@ rows = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   
 def display_board(ttt)
   puts " #{ttt[0]} | #{ttt[1]} | #{ttt[2]} "
   puts "-----------"
-  puts " #{ttt[3]} | #{ttt[4]} | #{ttt[5]}"
+  puts " #{ttt[3]} | #{ttt[4]} | #{ttt[5]} "
   puts "-----------"
-  puts " #{ttt[6]} | #{ttt[7]} | #{ttt[8]}"
+  puts " #{ttt[6]} | #{ttt[7]} | #{ttt[8]} "
 end
 
 display_board(board)
